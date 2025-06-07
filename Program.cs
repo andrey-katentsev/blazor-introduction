3a209a3a02fdc68a047c1d20a64ee2c2e2b9a378
Andrey Katentsev <akatentsev@gmail.com>
2025-06-07 14:05:04 +0200

    Entry point added.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+
+using blazor;
+using EventEase.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddSingleton<EventService>();
+builder.Services.AddSingleton<UserService>();
+
+await builder.Build().RunAsync();
